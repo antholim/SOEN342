0eee364846b4ff4fc5e1934fd679f064e2f3b2cc
Andrew Ungureanu <andrewrazvanu@gmail.com>
2025-10-07 18:35:40 -0400

    Changed structure again by adding a model directory, also added getters and setters

--- src/service/Csv.java
+++ src/service/Csv.java
@@ -1,5 +1,13 @@
 package service;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Records;
+
 
 public class Csv {
     
+
 }
