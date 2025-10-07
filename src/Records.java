763df946679b613894d52bf59b9c91a6f690ce85
Andrew Ungureanu <andrewrazvanu@gmail.com>
2025-10-07 18:05:59 -0400

    New branch

--- src/Records.java
+++ src/Records.java
@@ -1,3 +1,26 @@
 public class Records {
-    
+    private String routeId;
+    private String departureCity;
+    private String arrivalCity;
+    private String departureTime;
+    private String arrivalTime;
+    private String trainType;
+    private String daysOfOperation;
+    private double firstClassRate;
+    private double secondClassRate;
+
+    public Records(String routeId, String departureCity, String arrivalCity,
+                      String departureTime, String arrivalTime,
+                      String trainType, String daysOfOperation,
+                      double firstClassRate, double secondClassRate) {
+        this.routeId = routeId;
+        this.departureCity = departureCity;
+        this.arrivalCity = arrivalCity;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.trainType = trainType;
+        this.daysOfOperation = daysOfOperation;
+        this.firstClassRate = firstClassRate;
+        this.secondClassRate = secondClassRate;
+    }
 }
