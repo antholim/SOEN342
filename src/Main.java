d70c92ba946dd20670710c7f4387f2a2476c951a
antholim <139510082+antholim@users.noreply.github.com>
2025-10-08 00:25:11 -0400

    data package

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         CSVRepository repository = CSVRepository.getInstance();
-        List<Map<Records, String>> listOfRoutes =  repository.getRoutes("src/eu_rail_network.csv");
+        List<Map<Records, String>> listOfRoutes =  repository.getRoutes("src/data/eu_rail_network.csv");
         for (Map<Records, String> route : listOfRoutes) {
             System.out.println(route);
         }
